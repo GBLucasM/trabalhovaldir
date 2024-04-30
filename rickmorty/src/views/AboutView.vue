b20524235a7fdd61a49b5cc79ef04d2aacb642db
GBLucasM <152931750+GBLucasM@users.noreply.github.com>
2024-04-30 18:50:48 -0300

    Add files via upload

---
+++ rickmorty/src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h3>Dados do Aluno</h3>
+    <p>Aluno: Lucas Manoel Domingos de Lima</p>
+    <p>Universidade: Unimar</p>
+    <p>Curso: Análise de Desenvolvimento de Sistemas</p>
+    <p>RA: 1965323</p>
+
+  </main>
+</template>
